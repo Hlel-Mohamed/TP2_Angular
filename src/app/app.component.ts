c275a57ac3dc87589a792d9a29831165e59f34c1
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2024-05-01 11:23:57 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CarteVisiteComponent } from "./carte-visite/carte-visite.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CarteVisiteComponent]
+})
+export class AppComponent {
+  title = 'TP2_Angular';
+}
